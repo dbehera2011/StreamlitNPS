4f0282cf5752d325b83cb9d0f444422751865935
Smita <dbehera2011@gmail.com>
2026-04-06 00:14:55 +0530

    connection to local db

--- NPS_project.py
+++ NPS_project.py
@@ -9,6 +9,13 @@ import mysql.connector
 
 st.title("NPS Calculator!")
 
+# Initialize connection.
+conn = st.connection('mysql', type='sql')
+
+# Perform query.
+df = conn.query('SELECT * from survey;')
+
+################################################################################################
 # Connect to local MySQL
 # conn = mysql.connector.connect(
 #     host="127.0.0.1",       # or "127.0.0.1"
@@ -31,52 +38,52 @@ st.title("NPS Calculator!")
 # cursor.close()
 # conn.close()
 
+###################################################################################################
 
 
+# engine = create_engine("mysql+pymysql://root:aadi123@127.0.0.1:3306/smita_database")
 
-engine = create_engine("mysql+pymysql://root:aadi123@127.0.0.1:3306/smita_database")
+# #Run query with pandas to get both score & userId for Visualisation using MATLABLIB
+# df_visualisation = pd.read_sql("SELECT user_id, score FROM survey WHERE date = CURDATE()", engine)
+# st.write("Scores for Today ", df_visualisation)
 
-#Run query with pandas to get both score & userId for Visualisation using MATLABLIB
-df_visualisation = pd.read_sql("SELECT user_id, score FROM survey WHERE date = CURDATE()", engine)
-st.write("Scores for Today ", df_visualisation)
+# # Get today's date
+# today = datetime.date.today()
 
-# Get today's date
-today = datetime.date.today()
+# fig, ax = plt.subplots()
+# ax.plot(df_visualisation.user_id, df_visualisation.score) 
+# ax.set_ylabel('Score')
+# ax.set_xlabel('User ID')
+# ax.grid()
+# #Python’s f‑strings, short for formatted string literals.
+# ax.set_title(f"Today's User Ratings ({today})")
+# st.pyplot(fig)
 
-fig, ax = plt.subplots()
-ax.plot(df_visualisation.user_id, df_visualisation.score) 
-ax.set_ylabel('Score')
-ax.set_xlabel('User ID')
-ax.grid()
-#Python’s f‑strings, short for formatted string literals.
-ax.set_title(f"Today's User Ratings ({today})")
-st.pyplot(fig)
+# #Run query with pandas for NPS calculation
+# df_nps = pd.read_sql("SELECT score FROM survey WHERE date = CURDATE()", engine)
 
-#Run query with pandas for NPS calculation
-df_nps = pd.read_sql("SELECT score FROM survey WHERE date = CURDATE()", engine)
+# # Convert to NumPy array
+# data = df_nps.to_numpy()
 
-# Convert to NumPy array
-data = df_nps.to_numpy()
+# # Save as .npy file
+# np.save("survey.npy", data)
 
-# Save as .npy file
-np.save("survey.npy", data)
+# # Later, load it back with np.load
+# score = np.load("survey.npy", allow_pickle = True)
+# len(score)
 
-# Later, load it back with np.load
-score = np.load("survey.npy", allow_pickle = True)
-len(score)
+# #Calculate Detractor %
+# detractors = score[score<=6]
+# len(detractors)
+# percentage_detractors = (len(detractors)/len(score))*100
 
-#Calculate Detractor %
-detractors = score[score<=6]
-len(detractors)
-percentage_detractors = (len(detractors)/len(score))*100
+# #Calculate Promoter %
+# promoters = score[score>=9]
+# len(promoters)
+# percentage_promoters = (len(promoters)/len(score))*100
 
-#Calculate Promoter %
-promoters = score[score>=9]
-len(promoters)
-percentage_promoters = (len(promoters)/len(score))*100
-
-#NPS CAlculation
-NPS = percentage_promoters - percentage_detractors
-st.write("Today's NPS for the company is", NPS)
+# #NPS CAlculation
+# NPS = percentage_promoters - percentage_detractors
+# st.write("Today's NPS for the company is", NPS)
 
 
