0dd490c47b455764ce176771e4e1dac78d3e74b4
Smita <dbehera2011@gmail.com>
2026-04-06 20:40:29 +0530

    new column to survey

--- NPS_project.py
+++ NPS_project.py
@@ -80,8 +80,7 @@ df1 = pd.read_sql("SELECT * FROM survey", con=conn1.engine)
 df1['user_type'] = df1['score'].apply(assign_user_type)
 st.write("df_nps_today new column: ", df1)
 
- df2 = df1['user_type'].value_counts()
-
+df2 = df1['user_type'].value_counts()
 st.write(df2)
 
 
