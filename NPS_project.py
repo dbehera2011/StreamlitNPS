ad078bef294dd9333bbfe707228fb178c9c8f47d
Smita <dbehera2011@gmail.com>
2026-04-07 14:22:44 +0530

    new column to survey

--- NPS_project.py
+++ NPS_project.py
@@ -26,7 +26,7 @@ df_nps_today = pd.read_sql("SELECT user_id, score FROM nps_survey WHERE date = C
 
 # Get today's date
 today = datetime.date.today()
-#st.write(f"Scores for Today ({today})", df_nps_today)
+st.write(f"Scores for Today ({today})", df_nps_today)
 
 fig, ax = plt.subplots()
 ax.plot(df_nps_today.user_id, df_nps_today.score) 
@@ -35,7 +35,7 @@ ax.set_xlabel('User ID')
 ax.grid()
 #Python’s f‑strings, short for formatted string literals.
 ax.set_title(f"Today's User Ratings ({today})")
-#st.pyplot(fig)
+st.pyplot(fig)
 
 # Convert to NumPy array
 data = df_nps_today.score.to_numpy()
@@ -66,8 +66,11 @@ NPS = percentage_promoters - percentage_detractors
 #######################################################################
 #REQUIREMENT 1:
 #add new column user_type
-#	promoter >= 9, detractor <= 6, passives <= 6 & >= 9
-#2. Draw the graph bar chat using Seaborn
+  #promoter >= 9, detractor <= 6, passives <= 6 & >= 9
+#Draw the graph bar chat using Seaborn
+
+#REQUIREMENT 2:
+
 #######################################################################
 def assign_user_type(score):
   if score <= 6: return 'detractor'
